7a5bd2ed87a7798dd43b54bf9fa6e6de623823ff
evgeniy.pivovarchik <crashfacer.prol@gmail.com>
2024-01-29 17:19:21 +0300

    updated scipt dbo creating and schemes

--- Project/create_db_tables_keys.sql
+++ Project/create_db_tables_keys.sql
@@ -2,23 +2,25 @@ CREATE DATABASE GymProject
 
 CREATE TABLE Filials
 (
-    FilialsId	INT IDENTITY(1,1),
+    FilialId	INT IDENTITY(1,1),
 	Name NVARCHAR(50) NOT NULL,
 	Address NVARCHAR(100) NOT NULL,
-	PRIMARY KEY (FilialsId)
+	PRIMARY KEY (FilialId)
 );
 
-CREATE TABLE VisitClients
+CREATE TABLE Employees
 (
-    VisitClientId	INT IDENTITY(1,1),
-	ClientSubID INT,
-	VisitDate DATE NOT NULL,
-	FilialsID INT,
-	VisitEmployeeID INT,
-	PRIMARY KEY (VisitClientId),
-	FOREIGN KEY (ClientSubID) REFERENCES ClientsSubs(ClientSubID),
-	FOREIGN KEY (FilialsID) REFERENCES Filials(FilialsID),
-	FOREIGN KEY (VisitEmployeeID) REFERENCES VisitEmployees(VisitEmployeeID)
+	EmployeeID INT IDENTITY(1,1),
+    FirstName NVARCHAR(30),
+	LastName NVARCHAR(30) NOT NULL,
+	PassportNumber NVARCHAR(14) NOT NULL UNIQUE,
+	PhoneNumber NVARCHAR(15) NOT NULL,
+	Category INT NOT NULL,
+	Position NVARCHAR(50) NOT NULL,
+	SalaryPerHour MONEY NOT NULL CHECK (SalaryPerHour > 0),
+	PersonalTrainCost MONEY DEFAULT 0,
+	OrganizationRate FLOAT DEFAULT 0,
+	PRIMARY KEY (EmployeeID)
 );
 
 CREATE TABLE VisitEmployees
@@ -27,42 +29,43 @@ CREATE TABLE VisitEmployees
 	EmployeeID INT,
 	DateStart DATETIME NOT NULL,
 	DateEnd DATETIME,
-	FilialsID INT,
+	FilialID INT,
 	PRIMARY KEY (VisitEmployeeId),
-	FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID)
-	FOREIGN KEY (FilialsID) REFERENCES Filials(FilialsID)
+	FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID),
+	FOREIGN KEY (FilialID) REFERENCES Filials(FilialID)
 );
 
 CREATE TABLE ClientsSubs
 (
-    ClientsSubsID	INT IDENTITY(1,1),
+    ClientSubID	INT IDENTITY(1,1),
 	PassportNumber NVARCHAR(14) NOT NULL UNIQUE,
 	RegistrationDate DATE NOT NULL,
 	FirstName NVARCHAR(30) NOT NULL,
 	LastName NVARCHAR(30) NOT NULL,
 	PhoneNumber NVARCHAR(15) NOT NULL,
 	Email NVARCHAR(50) DEFAULT 'N/A',
-	SubscriptionDateStart DATE NOT NULL,
+	SubscriptionDateStart DATE NOT NULL DEFAULT '31.12.9999',
 	SubscriptionDateEnd DATE,
 	SubscriptionDatePurchase DATE NOT NULL,
 	SubcriptionName NVARCHAR(50) NOT NULL,
 	Duration INT NOT NULL,
-	Cost MONEY > 0 NOT NULL,
+	Cost MONEY NOT NULL CHECK (Cost > 0),
 	StatusSubscription NVARCHAR(20) NOT NULL,
-	PRIMARY KEY (ClientsSubsID)
+	PRIMARY KEY (ClientSubID)
 );
 
-CREATE TABLE Employees
+
+CREATE TABLE VisitClients
 (
-	EmplyeeID INT IDENTITY(1,1),
-    FirstName NVARCHAR(30),
-	LastName NVARCHAR(30) NOT NULL,
-	PassportNumber NVARCHAR(14) NOT NULL UNIQUE,
-	PhoneNumber NVARCHAR(15) NOT NULL,
-	Category INT NOT NULL,
-	Position NVARCHAR(50) NOT NULL,
-	SalaryPerHour MONEY > 0 NOT NULL,
-	PersonalTrainCost MONEY DEFAULT 0,
-	OrganizationRate FLOAT DEFAULT 0,
-	PRIMARY KEY (EmplyeeID)
+    VisitClientId	INT IDENTITY(1,1),
+	ClientSubID INT,
+	VisitDate DATE NOT NULL,
+	FilialID INT,
+	VisitEmployeeID INT,
+	PRIMARY KEY (VisitClientId),
+	FOREIGN KEY (ClientSubID) REFERENCES ClientsSubs(ClientSubID),
+	FOREIGN KEY (FilialID) REFERENCES Filials(FilialID),
+	FOREIGN KEY (VisitEmployeeID) REFERENCES VisitEmployees(VisitEmployeeID)
 );
+
+
